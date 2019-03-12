5ca7a4940bc8e8284b395bac891ac459c962a801
leo_d <leo_d@192.168.0.10>
2019-03-12 00:03:33 -0400

    2019-03-12

---
+++ src/pkg2019/pkg02/pkg15_helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2019.pkg02.pkg15_helloworld;
+
+/**
+ *
+ * @author leo_d
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World, this is Julia!");
+    }
+    
+}
